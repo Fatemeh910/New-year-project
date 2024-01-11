64a16ce9c4c0618dd688401a8d22757acd286c43
Fatemeh910 <fatemehkhoshnod91@gmail.com>
2024-01-11 13:01:54 +0330

    Gereftane fasele zamani

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const counter = () =>{
 const newYearDate = new Date("1 jan 2024").getTime();
 const currentDate = new Date().getTime();
+
+const diffrentTime = newYearDate - currentDate;
 };
 
-const interval = setInterval(counter,1000);
+const interval = setInterval(counter,1000); 
